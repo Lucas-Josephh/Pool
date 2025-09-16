443cbb6dccd7b089a030f57f989f298b2e20499e
Lucas <57141911+Lucas-Josephh@users.noreply.github.com>
2025-09-16 10:36:48 +0200

    Delete Day02_09_09_2025/Task_4.2.py

--- Day02_09_09_2025/Task_4.2.py
+++
@@ -1,9 +0,0 @@
-# Récursion des 6 premiers chiffres de PI
-
-def irrational(var, result) :
-    if var < 1 : return
-    result = var**2 / (6 + (var+2)**2)
-    irrational(var-2, result)
-    print(result+3)
-
-irrational(55, 0)
