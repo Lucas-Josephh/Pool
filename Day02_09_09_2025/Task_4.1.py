9e2803f024b98b93ac39182e2e2a451d92cf89ff
Lucas <57141911+Lucas-Josephh@users.noreply.github.com>
2025-09-16 10:36:42 +0200

    Delete Day02_09_09_2025/Task_4.1.py

--- Day02_09_09_2025/Task_4.1.py
+++
@@ -1,9 +0,0 @@
-# Récursion des 6 premiers chiffres de PI
-
-def SixFirstPi(n, result) :
-    if n == 1 : return
-    result = result + 4*(1/n)
-    SixFirstPi(n-2, result)
-    print(result)
-
-SixFirstPi(11, 0)
