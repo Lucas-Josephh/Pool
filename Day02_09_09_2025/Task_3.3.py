1c6bd01f15c70e48757ba588879892116cf557eb
Lucas <57141911+Lucas-Josephh@users.noreply.github.com>
2025-09-16 10:36:23 +0200

    Delete Day02_09_09_2025/Task_3.3.py

--- Day02_09_09_2025/Task_3.3.py
+++
@@ -1,9 +0,0 @@
-# Faire la somme des chiffres d'un nombre
-
-def sumDigit(n) :
-    result = 0
-    for i in range(0,len(str(n))) :
-        result = result + int(str(n)[i])
-    print("Somme des chiffre : ", result)
-
-sumDigit(123434565)
